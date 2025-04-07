78a0e9bf40df6220761cb04b0a4c1a783c10710c
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-07 23:00:39 +0530

    JS File

---
+++ JavaScript/Operators/Arithmatic Operators/myScript.js
@@ -0,0 +1,11 @@
+let num1 = 50;
+let num2 = 20;
+
+console.log("1st Number :",num1);
+console.log("2nd Number :",num2);
+
+console.log("Addition :",num1 + num2);
+console.log("Subtraction :",num1 - num2);
+console.log("Multiplication :",num1 * num2);
+console.log("Division :",num1 / num2);
+console.log("Modulo :",num1 % num2);
