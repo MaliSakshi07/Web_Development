52eada6d15c2c9abcf031d4f7dc1effa36bd1a7a
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-08 22:12:30 +0530

    Html File

---
+++ JavaScript/Operators/Assignment Operator/myScript.js
@@ -0,0 +1,9 @@
+let no1 = 20;
+let no2 = 10;
+
+console.log("Number 1 :",no1);
+console.log("Number 2 :",no2);
+console.log("Addition of",no1,"And",no2,":",no1 += no2);
+console.log("Subtraction of",no1,"And",no2,":",no1 -= no2);
+console.log("Multiplication of",no1,"And",no2,":",no1 *= no2);
+console.log("Division of",no1,"And",no2,":",no1 /= no2);
