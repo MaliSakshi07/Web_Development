49a98b011e1efda430505559d72e9668ee23fc44
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-09 23:06:44 +0530

    JS File

---
+++ JavaScript/Operators/Comparision Operator/myScript.js
@@ -0,0 +1,10 @@
+let no1 = 30;
+let no2 = 10;
+
+if(no1 === no2)
+{
+    console.log("Both",no1,"And",no2,"Are Equal Numbers.");
+}
+else{
+    console.log(no1,"And",no2,"Are Not Equal Numbers.");
+}
