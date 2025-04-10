fbe578e83c71dd33e1e07000b92bf2906c1cfa9b
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-10 23:10:06 +0530

    JS File

---
+++ JavaScript/Operators/Logical Operator/myScript.js
@@ -0,0 +1,9 @@
+let no1 = 20;
+let no2 = 40;
+
+let cond1 = no1 > no2;
+let cond2 = no1 === 20;
+
+console.log("cond1 && cond2 :",cond1 && cond2);
+console.log("cond1 || cond2 :",cond1 || cond2);
+console.log("!(cond1 && cond2) :", !(cond1 && cond2));
