5c830c2cdeda5cf6bbb95f25bea82be38784f61c
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-22 21:42:32 +0530

    JS File

---
+++ JavaScript/Array/Array-Loop.js
@@ -0,0 +1,7 @@
+let subject = ["Maths","Physics","Chemistry","Computer Science","English"];
+
+for(let i = 0; i<subject.length; i++){
+
+    console.log(subject[i]);
+}
+console.log(subject.length);
