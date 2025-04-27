34b896a3bcdb82c848f25a72936c5e73a6b6f555
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-27 21:59:07 +0530

    JS File

---
+++ JavaScript/Function/AdditionOf TwoNumber.js
@@ -0,0 +1,11 @@
+function addition(no1,no2){
+    Sum = no1+no2;
+
+    return Sum;
+}
+
+let val = addition(10,20);
+
+console.log("Number1 :",10);
+console.log("Number2 :",20);
+console.log("Addition =",val);
